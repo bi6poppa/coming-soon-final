b58ab62fd97a0b3ecd37727414d779f61b4b6dff
BigPoppa <support@paynedevelops.net>
2025-01-16 11:44:20 -0500

    added request form

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ import Clock from '@components/Clock/Clock'
 import Notify from '@components/Notify/Notify'
 import Socials from '@components/Socials/Socials'
 import CSFooter from '@components/CSFooter/CSFooter'
+import Nav from 'react-bootstrap/Nav'
 
 import 'bootstrap/dist/css/bootstrap.min.css'
 import './App.css'
@@ -18,12 +19,15 @@ function App() {
     <>  
         <Container className="p-0 m-0" fluid>
             <CSHeader />
-            <Row className="blankRows"><br className="blankRows" /></Row>
-            <Row className="p-0" id="row">
+            <Row className="py-4" id="row">
                 <Col className="p-0 m-0 col-md-6">
-                    <h1 id="website">Our new website<br />is on its way.</h1>
+                    <h1 id="website" className="pb-3">Our new website<br />is on its way.</h1>
                     <Countdown endDateTime={currentTime.setDate(currentTime.getDate() + 44)} />
-                    <h2 className="py-5" id="launch">Get notified when we launch.</h2>
+                    <h3 className="mt-3 pt-3 pb-2 mb-0" id="request">Need to submit a design request?</h3>
+                    <Row className="w-75">
+                        <Nav.Link className="justify-content-center fs-3 align-items-center" id="formLink" href="requestform.html">• Click Here •</Nav.Link>
+                    </Row>
+                    <h2 className="pt-3 mb-3 pb-3" id="launch">Get notified when we launch.</h2>
                     <Notify />
                     <Socials />
                 </Col>
@@ -31,7 +35,6 @@ function App() {
                     <Clock />
                 </Col>
             </Row>
-            <Row className="blankRows"><br className="blankRows" /></Row>
             <CSFooter />
         </Container>
     </>
---
+++ src/components/RequestForm/RequestForm.jsx
@@ -0,0 +1,18 @@
+import styles from './RequestForm.module.css'
+
+function RequestForm() {
+  (function(h,b,s,n,i,p,e,t) {
+    h._HB_ = h._HB_ || {};h._HB_.pid = i;;;;
+    t=b.createElement(s);t.type="text/javascript";t.async=!0;t.src=n;
+    e=b.getElementsByTagName(s)[0];e.parentNode.insertBefore(t,e);
+  })(window,document,"script","https://widget.honeybook.com/assets_users_production/websiteplacements/placement-controller.min.js","677d3bd90ebad6000707133a");
+
+  return (
+    <>
+      <div className="hb-p-677d3bd90ebad6000707133a-1" />
+      <img height="1" width="1" id={styles.hiddenImage} src="https://www.honeybook.com/p.png?pid=677d3bd90ebad6000707133a" />
+    </>
+  )
+}
+
+export default RequestForm;
